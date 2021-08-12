950c03a1eca32e1688996204309d977b2841601e
Dell <Dell@DESKTOP-RH71AQH>
2021-08-11 19:24:58 -0500

    Iniciando Crud tabla llave foranea producto

---
+++ ASPProyectoTercerTrimestre/Controllers/ProductoController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPProyectoTercerTrimestre.Models;
+
+namespace ASPProyectoTercerTrimestre.Controllers
+{
+    public class ProductoController : Controller
+    {
+        // GET: Producto
+        public ActionResult Index()
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return View(db.producto.ToList());
+            }
+            
+        }
+
+        public static string NombreProveedor(int idProveedor)
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return db.proveedor.Find(idProveedor).nombre;
+            }
+        }
+
+
+        public ActionResult ListarProveedores()
+        {
+            using(var db = new inventario2021Entities())
+            {
+                return PartialView(db.proveedor.ToList());
+            }
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(producto producto)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario2021Entities())
+                {
+                    db.producto.Add(producto);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+    }
+}
