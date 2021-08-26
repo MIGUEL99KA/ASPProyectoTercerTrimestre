8d6b051ce9ec49ccfbc68aa138543422235fe795
Dell <Dell@DESKTOP-RH71AQH>
2021-08-25 20:17:58 -0500

    Proceso de Login finzalizado

--- ASPProyectoTercerTrimestre/Controllers/UsuarioController.cs
+++ ASPProyectoTercerTrimestre/Controllers/UsuarioController.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using ASPProyectoTercerTrimestre.Models;
+using System.Web.Security;
+using System.Text;
 
 namespace ASPProyectoTercerTrimestre.Controllers
 {
+    [Authorize]
     public class UsuarioController : Controller
     {
         // GET: Usuario
@@ -36,6 +39,7 @@ namespace ASPProyectoTercerTrimestre.Controllers
             {
                 using (var db = new inventario2021Entities())
                 {
+                    usuario.password = UsuarioController.HashSHA1(usuario.password);
                     db.usuario.Add(usuario);
                     db.SaveChanges();
                     return RedirectToAction("Index");
@@ -45,6 +49,20 @@ namespace ASPProyectoTercerTrimestre.Controllers
                 ModelState.AddModelError("", "error " + ex);
                 return View();
             }
+        }
+
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
 
         public ActionResult Details(int id)
@@ -115,6 +133,48 @@ namespace ASPProyectoTercerTrimestre.Controllers
                 ModelState.AddModelError("", "error " + ex);
                 return View();
             }
+        }
+
+        public ActionResult Login( string mensaje = "")
+        {
+            ViewBag.Message = mensaje;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Login(string user, string password)
+        {
+            try
+            {
+                string passEncrip = UsuarioController.HashSHA1(password);
+                using (var db = new inventario2021Entities())
+                {
+                    var userLogin = db.usuario.FirstOrDefault(e => e.email == user && e.password == passEncrip);
+                    if(userLogin != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(userLogin.email, true);
+                        Session["User"] = userLogin;
+                        return RedirectToAction("index");
+                    }
+                    else
+                    {
+                        return Login("Verifique sus datos");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult CloseSession()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
